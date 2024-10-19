5b112a5020fae6e5544bc73a0885a562b7a4497a
Mohith Reddy Vuyyuru <mohithreddy2002@gmail.com>
2024-10-19 04:40:49 +0000

    fix bug4 View More

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,7 @@ export function App() {
           onChange={async (newValue) => {
             if (newValue === null) {
               return
-            } else if (newValue.id == "") {
+            } else if (newValue.id === "") {
               await loadAllTransactions()
             } else await loadTransactionsByEmployee(newValue.id)
           }}
